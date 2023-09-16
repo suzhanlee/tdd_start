4a71a42ad81c6b4ac77885221d9a8c0ade7f5cf7
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-09-16 18:41:56 +0900

    first commit

---
+++ src/main/java/com/example/tdd_start/TddStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdd_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddStartApplication.class, args);
+    }
+
+}
